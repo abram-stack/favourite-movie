1ec84f025d4e654268c375cd85624c27d9a545f1
abram-stack <anthonybramantyo@outlook.com>
2023-09-25 14:03:40 +0200

    fix API call. Get movieDetail

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const searchFormEl = document.getElementById('searchForm')
+const searchFormEl = document.getElementById('searchForm');
+const movieTitleEl = document.getElementById('movieTitle');
+const searchBtnEl = document.getElementById('searchBtn');
 
-searchFormEl.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const searchFormData = new FormData(searchFormEl)
-  const title = searchFormData.get('movieTitle')
-  if (!title) {
-    console.log('empty bruf')
+searchBtnEl.addEventListener('click', handleSearch);
+
+async function handleSearch() {
+  if (movieTitleEl) {
+    // console.log(movieTitleEl.value)
+    const movies = await getMoviesByTitle(movieTitleEl.value);
+    movies.forEach((movie) => {
+      renderHtml(movie)
+      // renderhtml() every loop 
+        // in renderhtml() we req to API for movie detail by ID
+    });
   }
-  console.log(title)
-})
+}
+
+async function getMoviesByTitle(title) {
+  const res = await fetch(
+    `https://www.omdbapi.com/?apikey=616b3029&s=${title}`
+  );
+  // const res = await fetch(`https://www.omdbapi.com/?apikey=616b3029&t=${title}`)
+  const data = await res.json();
+
+  return data.Search;
+}
+
+async function renderHtml(movie) {
+  let htmlString = ''
+  
+  const res = await fetch(`https://www.omdbapi.com/?apikey=616b3029&i=${movie.imdbID}`)
+  const movieDetails = await res.json()
+
+  const { Title, Poster, Runtime, imdbRating, Genre, Plot } = movieDetails
+  console.log(Title, Poster, Runtime, imdbRating, Genre)
+}
